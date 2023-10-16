32634213b79997ba25b2e6bca01183ebb4981754
Mike Marquezini <mike.santosmarquezini@ab-inbev.com>
2023-10-16 16:37:56 -0300

    update DOM

--- script.js
+++ script.js
@@ -16,15 +16,32 @@
 // }, 1000);
 
 
-const p = document.querySelectorAll('p')
+// const p = document.querySelectorAll('p')
 
-p.forEach((element, index)=> {
-    element.style.color = "#FFF";
-    element.style.padding= "20px";
-    if(index === 0){
-        element.style.background = "blue";
-    }
-    if(index === 1){
-        element.style.background = "red";
-    }   
+// p.forEach((element, index)=> {
+//     element.style.color = "#FFF";
+//     element.style.padding= "20px";
+//     if(index === 0){
+//         element.style.background = "blue";
+//     }
+//     if(index === 1){
+//         element.style.background = "red";
+//     }   
+// });
+
+
+const p = document.querySelector('p')
+p.textContent = "Texto inserido via JS"
+p.innerText = "Texto inserido via JS"
+
+const div = document.querySelector('div')
+div.innerHTML = `${div.innerHTML} <p>Texto inserido via JS</p>`;
+
+const elementUl = document.querySelector('ul');
+[1,2,3].forEach((element)=>{
+    const elementLi = document.createElement('li');
+    elementLi.innerText = element;
+    elementUl.appendChild(elementLi); //adiciona o elemento filho
 });
+
+div.appendChild(elementUl); //adiciona o elemento filho
